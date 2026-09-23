7e9e5903a563c5a588dc362aa1e0a3c09255159c
Okechukwu Ikwunze <oikwunze@gmail.com>
2026-09-23 18:25:33 -0400

    Added introduction string

---
+++ introduction.py
@@ -0,0 +1,31 @@
+# Strings and Personal Introduction Program
+
+"""
+A string in Python is a sequence of characters used 
+to represent textual information. Strings can 
+contain letters, numbers, spaces, and symbols, 
+and they are normally placed inside quotation marks.
+
+"""
+
+name ="Okechukwu Ikwunze"
+city = "Bowie, Maryland"
+country = "United States"
+profession = "Technology Professional"
+education = "I am continuing to develop my skills in technology and programming."
+programming_language = "Python is one of the programming languages I am learning."
+interest = "I am interested in Artificial Intelligence and Machine Learning."
+hobby = "I enjoy learning about technology and creating new projects."
+favorite_topic = "Generative AI is one of my favorite technology topics."
+goal = "My goal is to become highly skilled in Artificial Intelligence and Machine Learning."
+
+print(name)
+print(city)
+print(country)
+print(profession)
+print(education)
+print(programming_language)
+print(interest)
+print(hobby)
+print(favorite_topic)
+print(goal)

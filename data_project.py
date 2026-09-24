9f4ae55fb62caf961f43913ba8fc94d5d9863094
Okechukwu Ikwunze <oikwunze@gmail.com>
2026-09-24 12:43:20 -0400

    updated data project

---
+++ data_project.py
@@ -0,0 +1,81 @@
+# This program demonstrates basic Python data structures
+
+# A variable name used to store a value.
+
+# Variable containing a string(Textual information)
+student_name = "Okechukwu"
+
+# Variable containing an integer(whole number)
+years_experience = 5
+
+# Variable containing a Boolean(true or false)
+is_python_instructor = True
+
+# List of topics I am learning(Ordered, changeable collection in square brackets)
+learning_topics = [
+    "Python",
+    "Machine Learning",
+    "Artificial Intelligence",
+    "GitHub"
+]
+
+# Tuple representing my location
+location = (
+    "Bowie",
+    "Maryland",
+    "USA"
+)
+
+# Dictionary containing my student information(Stores key-value pairs, curly brackets)
+student_details = {
+    "name": "Okechukwu",
+    "profession": "Python Instructor",
+    "course": "Python Programming",
+    "goal": "Become an AI Engineer"
+}
+
+# Set containing my unique skills
+unique_skills = {
+    "Python",
+    "Git",
+    "Machine Learning",
+    "Communication"
+}
+
+# List representing a task queue(Represents items processed in FIFO order)
+task_queue = [
+    "Study Python",
+    "Practice Coding",
+    "Complete Assignment",
+    "Record YouTube Video"
+]
+
+# Display the Student Learning Profile
+
+print("STUDENT LEARNING PROFILE")
+
+print("\nStudent Name:")
+print(student_name)
+
+print("\nYears of Experience:")
+print(years_experience)
+
+print("\nPython Instructor:")
+print(is_python_instructor)
+
+print("\nLearning Topics:")
+print(learning_topics)
+
+print("\nLocation:")
+print(location)
+
+print("\nStudent Details:")
+print(student_details)
+
+print("\nUnique Skills:")
+print(unique_skills)
+
+print("\nTask Queue:")
+print(task_queue)
+
+# In conclusion, Each structure is useful for representing different kinds of information depending on the requirements of the program.

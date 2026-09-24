f5e34098687a2cbc795ab1a7b0b8fdbd09a82a41
Okechukwu Ikwunze <oikwunze@gmail.com>
2026-09-24 12:12:14 -0400

    Add syudent profile code

---
+++ student_profile.py
@@ -0,0 +1,31 @@
+# Python program representing my student profile using variables and different basic data types.
+
+"""
+DATA TYPES
+A string represents textual information(eg "okechukwu").
+An integer represents a whole number without a decimal point(eg 23).
+A float represents a number containing a decimal point(eg 5.9).
+A Boolean represents one of two values.
+"""
+
+name = "Okechukwu"
+age = 25
+city = "Bowie"
+course = "Python Programming"
+profession = "Python Student"
+experience_years = 3
+height = 5.9
+graduation_year = 2026
+learning_goal = "Become an expert in Artificial Intelligence"
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("City:", city)
+print("Course:", course)
+print("Profession:", profession)
+print("Experience:", experience_years)
+print("Height:", height)
+print("Graduation Year:", graduation_year)
+print("Learning Goal:", learning_goal)
+print("Student:", is_student)
